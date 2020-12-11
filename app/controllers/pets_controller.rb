e96d1ef5c2d53b890e40c9671c22739745653678
Arodriguez1030 <arodriguez1030@gmail.com>
2020-12-10 20:23:47 -0500

    created route and started view for new Pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,30 @@
 class PetsController < ApplicationController
+
+    before_action :set_pet, only: [:show, :edit, :update]
+
+    def new
+        @pet = Pet.new
+    end
+
+    def create
+        @pet = Pet.new(pet_params)
+        if @pet.save
+            redirect_to @pet
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def set_pet
+        @pet = Pet.find(params[:id])
+    end
+
+    def pet_params
+        params.require(:pet).permit!
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to users_path(@user)
+            redirect_to @user
         else
             render :new
+        end
     end
 
     def show
