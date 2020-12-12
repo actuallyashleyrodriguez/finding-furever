a0882bc1ccf5efee653305f9ab271f6c521d667a
Arodriguez1030 <arodriguez1030@gmail.com>
2020-12-12 10:27:41 -0500

    created new admin view page and show page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,30 @@
 class AdminsController < ApplicationController
+
+    before_action :set_admin, only: [:show, :edit, :update]
+
+    def new
+        @admin = Admin.new
+    end
+
+    def create
+        @admin = Admin.new(admin_params)
+        if @admin.save
+            redirect_to @admin
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def set_admin
+        @admin = Admin.find(params[:id])
+    end
+
+    def admin_params
+        params.require(:admin).permit!
+    end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ApplicationRecord
     has_many :shelters
     has_many :pets, through: :shelters
+    
+    has_secure_password
 end
