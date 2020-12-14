58ab54e702f0bedf99ca39c5d28009adc4f2529e
Arodriguez1030 <arodriguez1030@gmail.com>
2020-12-14 14:49:31 -0500

    connected pet to shelter#show started route for new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationsController < ApplicationController
+    def new
+        @app = Application.new
+    end
+
+
+    private
+    def app_params
+        params.require(:shelter).permit!
+    end
 end
