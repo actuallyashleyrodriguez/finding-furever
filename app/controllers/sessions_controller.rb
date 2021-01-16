dbe01df892c5f6eba9812bf0810da981102cb17f
Arodriguez1030 <arodriguez1030@gmail.com>
2021-01-16 08:04:28 -0500

    completed user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,12 @@ class SessionsController < ApplicationController
     end
  
     def create
-        byebug
-        session[:email] = params[:email]
-        redirect_to '/users/:id'
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            redirect_to login_path
+        end
     end
 end
